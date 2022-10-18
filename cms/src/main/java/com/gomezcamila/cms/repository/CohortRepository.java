7ee62a1c1bd91ee757ae6f0aebdd386c760777f0
camilagx <camilac.gomez97@gmail.com>
2022-10-18 13:52:37 -0700

    Create Cohort Service and Repository

---
+++ cms/src/main/java/com/gomezcamila/cms/repository/CohortRepository.java
@@ -0,0 +1,10 @@
+package com.gomezcamila.cms.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.gomezcamila.cms.model.Cohort;
+
+public interface CohortRepository extends JpaRepository<Cohort, Long> {
+
+
+ 
+}
---
+++ cms/src/main/java/com/gomezcamila/cms/service/CohortService.java
@@ -0,0 +1,46 @@
+package com.gomezcamila.cms.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.gomezcamila.cms.model.Cohort;
+import com.gomezcamila.cms.repository.CohortRepository;
+
+@Service
+public class CohortService {
+	@Autowired
+	CohortRepository cohortRepository;
+	
+	//READ ONLY - Get All cohorts
+	public List<Cohort> getAllCohort(){
+		return cohortRepository.findAll();
+	}
+	//READ ONLY - Get Cohort by ID
+	public Cohort getCohortById(Long id){
+		return cohortRepository.findById(id).get();
+	}
+	
+	//Create
+	public Cohort saveCohort(Cohort cohort) {
+		Cohort newCohort = cohortRepository.findById(cohort.getCohortId()).get();
+		cohortRepository.save(newCohort);
+		
+		return newCohort;
+	}
+	//UPDATE
+	public Cohort updateCohort(Cohort cohort) {
+		Cohort updatedCohort = cohortRepository.findById(cohort.getCohortId()).get();
+		cohortRepository.save(updatedCohort);
+		
+		return updatedCohort;
+	}
+	//DELETE
+	public Cohort deleteCohortById(Long id) {
+		Cohort cohortDeleted = cohortRepository.findById(id).get();
+		cohortRepository.deleteById(id);
+		
+		
+		return cohortDeleted;
+	}
+}
